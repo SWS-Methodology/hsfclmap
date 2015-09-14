b19c7a2acb8437ae9cc1619990b34ff3447c476b
Alexander Matrunich <alexander@matrunich.com>
2015-09-14 11:50:26 +0200

    hsfclmap for 2009+

--- R/data.R
+++ R/data.R
@@ -1,5 +1,10 @@
-#' Mapping between FCL and country specific HS
+#' Most recent mapping between FCL and country specific HS
 #'
 #' @format A data frame
 #'
 "hsfclmap"
+
+#' Mappings between FCL and country specific HS starting from 2009.
+#'
+#' @format A data frame
+"hsfclmap2"
---
+++ data-raw/read_from_mdb.R
@@ -0,0 +1,82 @@
+# 2009 and above
+
+library(dplyr, warn.conflicts = F)
+library(stringr)
+
+readitemcodesmdb <- function(filename, table) {
+
+  # classes <- c("character", "character", "character", "character", "character", "character")
+
+  Hmisc::mdb.get(filename,
+                 colClasses = "character",
+                 stringsAsFactors = F,
+                 lowernames = T,
+                 tables = table) %>%
+    select_(~fromcode, ~tocode, fclorig = ~faostat, ~year) %>%
+    mutate_(fcl = ~as.integer(fclorig))
+}
+
+
+
+directory <- file.path("", "mnt", "essdata", "TradeSys", "TradeSys", "Countries")
+
+
+areas_years <- data.frame(dirnames = dir(directory, full.names = F), stringsAsFactors = F) %>%
+  filter(str_detect(dirnames, "^[A-Z]{3}_[1-2][0-9]{3}$")) %>%
+  mutate(area = str_extract(dirnames, "^[A-Z]{3}"),
+         year = as.integer(str_extract(dirnames, "[1-2][0-9]{3}$"))) %>%
+  filter(year >= 2009)
+
+hsfclmap2 <- bind_rows(plyr::ldply(areas_years$dirnames, function(dirname) {
+
+  area <- areas_years$area[areas_years$dirnames == dirname]
+  year <- areas_years$year[areas_years$dirnames == dirname]
+
+  if(dirname == "CLA_2002") dirname <- "ECU_2000"
+  if(dirname == "BYE_2013") dirname <- "AZE_2013"
+
+  filename <- file.path(directory, dirname, paste0(dirname, ".mdb"))
+
+  code1 <- readitemcodesmdb(filename, "ItemCode1")
+  code2 <- readitemcodesmdb(filename, "ItemCode2")
+
+  if(area == "INS" & year == 2013) code1$tocode[code1$tocode == "4058999"] <- "40589999"
+
+  areanames <- data.frame(
+    area = area,
+    stringsAsFactors = F)
+
+  code1 <- cbind(code1, areanames, flow = "Import", mdbyear = year, stringsAsFactors = F)
+  code2 <- cbind(code2, areanames, flow = "Export", mdbyear = year, stringsAsFactors = F)
+
+  if("FALSE" %in% code1$area) message(paste0(dirname, " code1 ", sum(df$area == "FALSE")))
+  if("FALSE" %in% code2$area) message(paste0(dirname, " code2 ", sum(df$area == "FALSE")))
+
+  df <- bind_rows(code1, code2) %>% as.data.frame
+  # df <- bind_rows(code1, code2, stringsAsFactors = F) %>% as.data.frame
+
+  df
+
+}, .progress = "text", .inform = T))
+
+## Read area codes / names from Onno's map
+
+
+codes <- XLConnect::readWorksheetFromFile(file.path("data-raw",
+                                                    "Trade_Country-Registry.xlsx"),
+                                          header = T,
+                                          sheet = "Sheet1",
+                                          endCol = 2)
+codes$Acronyme <- toupper(codes$Acronyme)
+names(codes) <- tolower(names(codes))
+codes <- rbind(codes, c("VIE", 237))
+
+hsfclmap2 <- hsfclmap2 %>%
+  left_join(codes %>%
+              rename_(fao = ~fao_code),
+            by = c("area" = "acronyme")) %>%
+  mutate(fao = as.integer(fao))
+
+save(hsfclmap2,
+     file = file.path("data",
+                      "hsfclmap2.rdata"))
