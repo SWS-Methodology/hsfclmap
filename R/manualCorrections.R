7a4c7fc1ef8c69b44db8da86d95686f7778a7a08
Alexander Matrunich <alexander@matrunich.com>
2015-10-29 12:38:23 +0100

    in hsfclmap country character names were changed to codes. And numeric characters converted to integers.

--- R/manualCorrections.R
+++ R/manualCorrections.R
@@ -1,20 +1,20 @@
 manualCorrections <- function(map) {
-  map$tocode[map$area == "PAL" &
+  map$tocode[map$area == 299 &
                map$fromcode == "29192" &
                map$tocode   == "19192" &
-               map$fcl      == "1293"] <- "29192"
+               map$fcl      == 1293] <- "29192"
 
 
   # In any case it is a duplicate
-  map$fromcode[map$area == "CAN" &
+  map$fromcode[map$area == 33 &
                  map$fromcode == "0708900000" &
                  map$tocode   == "0207430000" &
-                 map$fcl      == "1075"]  <- "0207430000"
+                 map$fcl      == 1075]  <- "0207430000"
 
-  map$fromcode[map$area == "BEL" &
+  map$fromcode[map$area == 255 &
                  map$fromcode == "210609030" &
                  map$tocode   == "21069030" &
-                 map$fcl      == "175"]  <- "21069030"
+                 map$fcl      == 175]  <- "21069030"
   map
 
 }
