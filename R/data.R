a54f5772efd21bcc88bb7decb2a94b1e9a150b9f
Alexander Matrunich <alexander@matrunich.com>
2015-12-03 15:13:01 +0100

    hsfclmap dataset removed from package

--- R/data.R
+++ R/data.R
@@ -1,9 +1,3 @@
-#' Most recent mapping between FCL and country specific HS
-#'
-#' @format A data frame
-#'
-"hsfclmap"
-
 #' Mappings between FCL and country specific HS starting from 2009.
 #'
 #' @format A data frame
