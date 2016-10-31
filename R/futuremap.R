6c30ffef1661eb8ac10262e5fd08025bfcd1f54f
Alexander Matrunich <alexander@matrunich.com>
2016-10-31 13:54:43 +0300

    A set of functions for extraction of unmapped codes from future years

---
+++ R/futuremap.R
@@ -0,0 +1,65 @@
+loadesdata <- function() {
+  readr::read_csv(
+    file.path(
+      Sys.getenv("HOME"),
+      "ce_combinednomenclature_unlogged_2014.csv"
+    ),
+    na = "NA",
+    skip = 1L,
+    col_types = "ccccii____",
+    col_names = c(
+      "chapter",
+      "reporter",
+      "partner",
+      "hs",
+      "flow",
+      "stat_regime"
+      )
+    ) %>% 
+    filter_(~chapter %in% c('01', '02', '03', '04', '05', '06', '07',
+                            '08', '09', '10', '11', '12', '13', '14',
+                            '15', '16', '17', '18', '19', '20', '21',
+                            '22', '23', '24', '33', '35', '38', '40',
+                            '41', '42', '43', '50', '51', '52', '53')) %>% 
+    filter_(~stat_regime == 4L) %>% 
+    assertr::verify(nrow(.) > 0) %>% 
+    select_(~-stat_regime, ~-chapter) %>% 
+    filter_(~stringr::str_detect(reporter,
+            "^[[:digit:]]+$")) %>%
+  filter_(~stringr::str_detect(partner,
+          "^[[:digit:]]+$")) %>%
+  filter_(~stringr::str_detect(hs,
+          "^[[:digit:]]+$")) %>%
+      assertr::verify(nrow(.) > 0) %>% 
+    select_(~-partner) %>% 
+    mutate_(reporter = ~as.numeric(reporter))
+                            
+}
+
+loadgeonom <- function() {
+  
+  file2save <- tempfile(fileext = ".RData")
+  
+  download.file(
+    "https://github.com/SWS-Methodology/faoswsTrade/blob/master/data/geonom2fao.RData?raw=true", 
+    destfile = file2save,
+    quiet = TRUE)
+  
+  load(file2save, envir = environment())
+  
+  file.remove(file2save)
+  
+  geonom2fao
+}
+
+esdata2faoarea <- function(data, g2fmap) {
+  
+  data %>% 
+    left_join(g2fmap %>% 
+                select_(geonom = ~code,
+                        fao    = ~active), 
+              by = c("reporter" = "geonom")) %>% 
+    select_(~-reporter) %>% 
+    rename_(reporter = ~fao)
+  
+}
