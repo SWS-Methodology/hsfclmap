5046b438d32f830ba4ec2967df614a16a0575fce
Alexander Matrunich <alexander@matrunich.com>
2016-10-31 20:10:31 +0300

    futuremap: functions modifed not to be year specific

--- R/futuremap.R
+++ R/futuremap.R
@@ -1,9 +1,8 @@
-loadesdata <- function() {
-  readr::read_csv(
-    file.path(
-      Sys.getenv("HOME"),
-      "ce_combinednomenclature_unlogged_2014.csv"
-    ),
+loadesdata <- function(file = file.path(
+  Sys.getenv("HOME"),
+  "ce_combinednomenclature_unlogged_2014.csv.gz")
+  ) {
+  readr::read_csv(file,
     na = "NA",
     skip = 1L,
     col_types = "ccccii____",
@@ -14,23 +13,24 @@ loadesdata <- function() {
       "hs",
       "flow",
       "stat_regime"
-      )
+    )
   ) %>% 
     filter_(~chapter %in%
               sprintf("%02d", c(1:24, 33, 35, 38, 40:43, 50:53))) %>% 
     filter_(~stat_regime == 4L) %>% 
     assertr::verify(nrow(.) > 0) %>% 
     select_(~-stat_regime, ~-chapter) %>% 
+    filter_(~stringr::str_length(hs) > 2) %>% 
     filter_(~stringr::str_detect(reporter,
-            "^[[:digit:]]+$")) %>%
-  filter_(~stringr::str_detect(partner,
-          "^[[:digit:]]+$")) %>%
-  filter_(~stringr::str_detect(hs,
-          "^[[:digit:]]+$")) %>%
-      assertr::verify(nrow(.) > 0) %>% 
+                                 "^[[:digit:]]+$")) %>%
+    filter_(~stringr::str_detect(partner,
+                                 "^[[:digit:]]+$")) %>%
+    filter_(~stringr::str_detect(hs,
+                                 "^[[:digit:]]+$")) %>%
+    assertr::verify(nrow(.) > 0) %>% 
     select_(~-partner) %>% 
     mutate_(reporter = ~as.numeric(reporter))
-                            
+  
 }
 
 loadgeonom <- function() {
