fcca42b2c5a903d7df398455026be3a713a043ce
Alexander Matrunich <alexander@matrunich.com>
2016-11-02 11:39:49 +0300

    hsInRange() documentation added

--- R/hsInRange.R
+++ R/hsInRange.R
@@ -1,13 +1,34 @@
 #' Looks for corresponding FCL codes in country-specific
 #' mapping tables from MDB files
 #'
+#' @param hs Numeric or character vector with HS codes to convert to FCL.
+#' @param areacode Numeric or character vector with reporters' codes.
+#' @param flowname Numeric or character vector of trade direction.
+#' @param mapdataset Data frame with HS->FCL mapping with columns area,
+#'   flow, fromcode, tocode, fcl
+#'   registered with doParallel package.
+#'   
+#' @return Data frame with columns id, area, flow, hsorig, hsext, fcl. 
+#'   id holds row numbers of original dataset. hsorig is input hs. hsext is 
+#'   input hs with additional zeros if requires. If there are multiple 
+#'   HS->FCL matchings, all of them are returned with similar id. If 
+#'   there were no matching FCL codes, NA in fcl column is returned.
+#'   
+#' @details Input hs, areacode and flowname columns are used to build 
+#'   data frame with hs-codes to convert. Probably it is easier to 
+#'   pass a data frame, then separate vectors.
+#'
 #' @import dplyr
 #' @export
 
 
-hsInRange <- function(hs, areacode, flowname, mapdataset,
+hsInRange <- function(hs, 
+                      areacode,
+                      flowname,
+                      mapdataset,
                       parallel = FALSE) {
   
+  # Constructing of data.frame with original hs codes.
   if(!all.equal(length(hs), 
                 length(areacode), 
                 length(flowname)))
@@ -18,6 +39,7 @@ hsInRange <- function(hs, areacode, flowname, mapdataset,
                    flowname = flowname) %>% 
     mutate(id = row_number())
   
+  # 
   df_fcl <- plyr::ddply(
     df,
     .variables = c("areacode", "flowname"),
