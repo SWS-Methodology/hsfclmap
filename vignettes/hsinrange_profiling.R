6eed8352fff86c0caa27d0112145f92ddcae2c1c
Alexander Matrunich <alexander@matrunich.com>
2016-11-02 11:06:23 +0300

    hsinrange_profiling: doParallel

--- vignettes/hsinrange_profiling.R
+++ vignettes/hsinrange_profiling.R
@@ -1,24 +1,53 @@
 library(stringr)
 library(dplyr, warn.coflicts = FALSE)
+library(foreach)
+library(doParallel)
+registerDoParallel(cores = detectCores(all.tests = TRUE))
 
 esdataorig <- loadesdata(file.path(
   Sys.getenv("HOME"), 
-  "ce_combinednomenclature_unlogged_2013.csv.gz"))
+  "ce_combinednomenclature_unlogged_2014.csv.gz"))
 
-esdata13 <- esdata2faoarea(esdataorig, loadgeonom())
+esdata14 <- esdata2faoarea(esdataorig, loadgeonom())
 
 smpl <- 10^5
 
-esdatafcl <- esdata %>% 
+esdatafcl14 <- esdata14 %>% 
   sample_n(smpl) %>% 
   do(hsInRange(.$hs, .$reporter, .$flow, 
                hsfclmap3 %>% 
-                 filter(str_detect(fromcode, "^\\d+$")))) 
+                 filter(str_detect(fromcode, "^\\d+$")),
+               parallel = TRUE)) 
   
+esdatafcl <- esdatafcl14
 
 nrow(esdatafcl) / smpl
 
 sum(is.na(esdatafcl$fcl)) / nrow(esdatafcl)
 
-sum(!unique(hsfclmap3$fcl) %in% unique(esdatafcl$fcl)) / length(unique(hsfclmap3$fcl))
+sum(unique(hsfclmap3$fcl) %in% 
+      unique(esdatafcl$fcl)) / 
+  length(unique(hsfclmap3$fcl))
 
+length(unique(hsfclmap3$fcl))
+length(unique(esdatafcl$fcl))
+
+esdatafcl %>% 
+  group_by(id) %>% 
+  summarize(manyfcl = length(unique(fcl)) > 1) %>% 
+  ungroup() %>% 
+  summarize(sum(manyfcl) / n())
+
+esdatafcl %>% 
+  group_by(id) %>% 
+  summarize(nofcl = any(is.na(fcl))) %>% 
+  ungroup() %>% 
+  summarize(sum(nofcl)/n())
+
+esdatafcl %>% 
+  group_by(area, id) %>% 
+  summarize(nofcl = any(is.na(fcl))) %>% 
+  group_by(area) %>% 
+  summarize(nofclprop = sum(nofcl)/n()) %>% 
+  ungroup() %>% 
+  arrange(desc(nofclprop))
